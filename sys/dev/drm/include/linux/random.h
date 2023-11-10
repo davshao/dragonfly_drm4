fc2a2e3507700681dff98616c9a88783fbccf1b2
David Shao <davshao@gmail.com>
2023-11-10 14:10:36 -0800

    Patch sys/dev/drm/include/linux/random.h for arc4rand_buf() and arc4random_uniform.c

--- sys/dev/drm/include/linux/random.h
+++ sys/dev/drm/include/linux/random.h
@@ -76,43 +76,6 @@ get_random_long(void)
 	return karc4random64();
 }
 
-/* From DragonFly lib/libc/gen/arc4random.c */
-/*
- * Calculate a uniformly distributed random number less than upper_bound
- * avoiding "modulo bias".
- *
- * Uniformity is achieved by generating new random numbers until the one
- * returned is outside the range [0, 2**32 % upper_bound).  This
- * guarantees the selected random number will be inside
- * [2**32 % upper_bound, 2**32) which maps back to [0, upper_bound)
- * after reduction modulo upper_bound.
- */
-static inline uint32_t
-karc4random_uniform(uint32_t upper_bound)
-{
-	uint32_t r, min;
-
-	if (upper_bound < 2)
-		return 0;
-
-	/* 2**32 % x == (2**32 - x) % x */
-	min = -upper_bound % upper_bound;
-
-	/*
-	 * This could theoretically loop forever but each retry has
-	 * p > 0.5 (worst case, usually far better) of selecting a
-	 * number inside the range we need, so it should rarely need
-	 * to re-roll.
-	 */
-	for (;;) {
-		r = karc4random();
-		if (r >= min)
-			break;
-	}
-
-	return r % upper_bound;
-}
-
 static inline uint32_t
 prandom_u32_max(uint32_t x)
 {
@@ -122,7 +85,7 @@ prandom_u32_max(uint32_t x)
 static inline void
 get_random_bytes(void *buf, int nbytes)
 {
-	karc4rand(buf, nbytes);
+	karc4random_buf(buf, nbytes);
 }
 
 #endif
